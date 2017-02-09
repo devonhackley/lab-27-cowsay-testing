e5df778a0ff76ad38d412648ff69a19180b60f56
devonhackley <devonhackley@gmail.com>
2017-02-08 17:16:10 -0800

    tests are passing, will try the bonus

---
+++ test/cowsay-test.js
@@ -0,0 +1,51 @@
+'use strict';
+
+require('angular-mocks');
+const expect = require('chai').expect;
+const cowsayBrowser = require('cowsay-browser');
+
+describe('testing cowsay component\'s controller', function() {
+  beforeEach(done => {
+    angular.mock.module('demoApp');
+    angular.mock.inject($componentController => {
+      this.$componentController = $componentController;
+      done();
+    });
+  });
+
+  beforeEach(done => {
+    this.cowsayCtrl = this.$componentController('cowsay');
+    this.cowsayCtrl.$onInit();
+    done();
+  });
+
+  describe('testing initial state', () => {
+    it('should initialize methods and properties', done => {
+      expect(this.cowsayCtrl.title).to.equal('Yay cows');
+      expect(this.cowsayCtrl.userInput).to.equal('');
+      expect(this.cowsayCtrl.history).to.be.instanceof(Array);
+      expect(this.cowsayCtrl.currentCow).to.be.equal('');
+      expect(this.cowsayCtrl.history.length).to.equal(0);
+      expect(typeof this.cowsayCtrl.getCow).to.equal('function');
+      expect(typeof this.cowsayCtrl.saveCow).to.equal('function');
+      expect(typeof this.cowsayCtrl.undoCow).to.equal('function');
+      done();
+    });
+  });
+
+  describe('testing getCow', () => {
+    it('should return a cow that says "Got milk?"', done => {
+      let result = this.cowsayCtrl.getCow();
+      expect(result).to.equal(cowsayBrowser.say({text: 'Got milk?'}));
+      done();
+    });
+
+    it('should return a cow that says MOOVE Over', done => {
+      this.cowsayCtrl.userInput = 'MOOVE Over';
+      let result = this.cowsayCtrl.getCow(this.cowsayCtrl.userInput);
+      expect(result).to.equal(cowsayBrowser.say({text: 'MOOVE Over'}));
+      done();
+    });
+  });
+
+});
